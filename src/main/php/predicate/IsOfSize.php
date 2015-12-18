19b0cac5856217bf9b2aec46b0dd19861da9e667
mikey179 <mikey@stubbles.net>
2015-12-18 13:35:18 +0100

    add predicate for checking the size of something

---
+++ src/main/php/predicate/IsOfSize.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file is part of bovigo\assert.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bovigo\assert\predicate;
+/**
+ * Predicate to test that something is of a certain size.
+ */
+class IsOfSize extends Predicate
+{
+    /**
+     * the expected size
+     *
+     * @type  int
+     */
+    private $expectedSize;
+
+    /**
+     * constructor
+     *
+     * @param  int   $expectedSize  value to which test size must be equal
+     */
+    public function __construct($expectedSize)
+    {
+        $this->expectedSize = $expectedSize;
+    }
+
+    /**
+     * test that the given value is of a certain size
+     *
+     * @param   string|array|\Countable|\Traversable  $value
+     * @return  bool   true if size of value is equal to expected size, else false
+     * @throws  \InvalidArgumentException
+     */
+    public function test($value)
+    {
+        if (is_string($value)) {
+            return strlen($value) === $this->expectedSize;
+        } elseif (is_array($value) || $value instanceof \Countable) {
+            return count($value) === $this->expectedSize;
+        } elseif ($value instanceof \Traversable) {
+            return iterator_count($this->cloneIterator($value)) === $this->expectedSize;
+        } else {
+            throw new \InvalidArgumentException(
+                    'Given value is neither a string, an array,'
+                    . ' nor an instance of \Countable or \Traversable,'
+                    . ' but of type ' . gettype($value)
+            );
+        }
+    }
+
+    /**
+     * clones given traversable
+     *
+     * We need to use a clone because iterator_count() moves the pointer of the
+     * iterator, but we don't want to change the pointer position of a passed
+     * iterator instance as this would violate functional principles.
+     *
+     * @param   \Traversable  $traversable
+     * @return  \Iterator
+     */
+    private function cloneIterator(\Traversable $traversable)
+    {
+        if ($traversable instanceof \IteratorAggregate) {
+            return clone $traversable->getIterator();
+        }
+
+        return clone $traversable;
+    }
+
+    /**
+     * returns string representation of predicate
+     *
+     * @return  string
+     */
+    public function __toString()
+    {
+        return sprintf(
+            'size matches %d',
+            $this->expectedSize
+        );
+    }
+}
---
+++ src/test/php/predicate/IsOfSizeTest.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * This file is part of bovigo\assert.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bovigo\assert\predicate;
+use function bovigo\assert\assert;
+/**
+ * Helper class for the test.
+ */
+class CountableExample implements \Countable
+{
+    public function count()
+    {
+        return 3;
+    }
+}
+/**
+ * Helper class for the test.
+ */
+class TraversableExample implements \Iterator
+{
+    private $current = 0;
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function key()
+    {
+        return $this->current;
+    }
+
+    public function next()
+    {
+        $this->current++;
+    }
+
+    public function rewind()
+    {
+        $this->current = 0;
+    }
+
+    public function valid()
+    {
+        return 3 > $this->current;
+    }
+
+}
+/**
+ * Helper class for the test.
+ */
+class IteratorAggregateExample implements \IteratorAggregate
+{
+    public function getIterator()
+    {
+        return new \ArrayIterator([1, 2, 3]);
+    }
+}
+/**
+ * Tests for bovigo\assert\predicate\IsOfSize.
+ *
+ * @group  predicate
+ */
+class IsOfSizeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function evaluatesToTrueIfStringLengthMatchesExpectedSize()
+    {
+        assert((new IsOfSize(3))->test('foo'), isTrue());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToFalseIfStringLengthDoesNotMatchExpectedSize()
+    {
+        assert((new IsOfSize(4))->test('foo'), isFalse());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToTrueIfArraySizeMatchesExpectedSize()
+    {
+        assert((new IsOfSize(3))->test([1, 2, 3]), isTrue());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToFalseIfArraySizeDoesNotMatchExpectedSize()
+    {
+        assert((new IsOfSize(4))->test([1, 2, 3]), isFalse());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToTrueIfCountableSizeMatchesExpectedSize()
+    {
+        assert((new IsOfSize(3))->test(new CountableExample()), isTrue());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToFalseIfCountableSizeDoesNotMatchExpectedSize()
+    {
+        assert((new IsOfSize(4))->test(new CountableExample()), isFalse());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToTrueIfTraversableSizeMatchesExpectedSize()
+    {
+        assert((new IsOfSize(3))->test(new TraversableExample()), isTrue());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToFalseIfTraversableSizeDoesNotMatchExpectedSize()
+    {
+        assert((new IsOfSize(4))->test(new TraversableExample()), isFalse());
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToTrueIfIteratorAggregateSizeMatchesExpectedSize()
+    {
+        assert(
+                (new IsOfSize(3))->test(new IteratorAggregateExample()),
+                isTrue()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function evaluatesToFalseIfIteratorAggregateSizeDoesNotMatchExpectedSize()
+    {
+        assert(
+                (new IsOfSize(4))->test(new IteratorAggregateExample()),
+                isFalse()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function iteratorPointerIsNotChangedByEvaluation()
+    {
+        $example = new TraversableExample();
+        $example->next();
+        (new IsOfSize(3))->test($example);
+        assert($example->current(), equals(1));
+    }
+
+    /**
+     * @test
+     * @expectedException  InvalidArgumentException
+     */
+    public function throwsInvalidArgumentExceptionWhenValueIsNotTestableForSize()
+    {
+        (new IsOfSize(3))->test(true);
+    }
+}
