19b9a7001419d5a40e95cc2851d31ab4af0a1820
mikey179 <mikey@stubbles.net>
2015-12-29 10:53:00 +0100

    add specific tests for assert functions

---
+++ src/test/php/AssertTest.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This file is part of bovigo\assert.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bovigo\assert;
+use function bovigo\assert\predicate\equals;
+use function bovigo\assert\predicate\isSameAs;
+use function bovigo\assert\predicate\isTrue;
+/**
+ * Tests for bovigo\assert\*().
+ *
+ * @group  assert
+ * @since  1.2.0
+ */
+class AssertTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function assertSucceedsWhenPredicateReturnsTrue()
+    {
+        assert(assert('some value', function() { return true; }), isTrue());
+    }
+
+    /**
+     * @test
+     * @expectedException  bovigo\assert\AssertionFailure
+     * @expectedExceptionMessage  Failed asserting that 'some value' satisfies a lambda function.
+     */
+    public function assertFailsWhenPredicateReturnsFalse()
+    {
+        assert('some value', function() { return false; });
+    }
+
+    /**
+     * @test
+     */
+    public function assertionFailureContainsAdditionalDescription()
+    {
+        try {
+            assert('some value', function() { return false; }, 'some more info');
+        } catch (AssertionFailure $af) {
+            assert(
+                    $af->getMessage(),
+                    equals('Failed asserting that \'some value\' satisfies a lambda function.
+some more info')
+            );
+            return;
+        }
+
+        $this->fail('Expected ' . AssertionFailure::class . ', gone none');
+    }
+
+    /**
+     * @test
+     */
+    public function exporterAlwaysReturnsSameInstance()
+    {
+        assert(exporter(), isSameAs(exporter()));
+    }
+
+    /**
+     * @test
+     */
+    public function assertionCounterIsIncreasedByAmountOfPredicatesUsedForAssertion()
+    {
+        if (!class_exists('\PHPUnit_Framework_Assert')) {
+            $this->skip('Can not test this without PHPUnit');
+        }
+
+        $countBeforeAssertion = \PHPUnit_Framework_Assert::getCount();
+        assert('some value', function() { return true; });
+        assert(
+                \PHPUnit_Framework_Assert::getCount(),
+                equals($countBeforeAssertion + 1)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function assertionCounterIsIncreasedInCaseOfFailure()
+    {
+        if (!class_exists('\PHPUnit_Framework_Assert')) {
+            $this->skip('Can not test this without PHPUnit');
+        }
+
+        $countBeforeAssertion = \PHPUnit_Framework_Assert::getCount();
+        try {
+            assert('some value', function() { return false; }, 'some more info');
+        } catch (AssertionFailure $af) {
+            assert(
+                    \PHPUnit_Framework_Assert::getCount(),
+                    equals($countBeforeAssertion + 1)
+            );
+            return;
+        }
+
+        $this->fail('Expected ' . AssertionFailure::class . ', gone none');
+    }
+}
